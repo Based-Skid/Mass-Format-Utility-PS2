4aa448c518b935d4d17bb9ab554337e596344f87
Nick Freeman <24757323+Based-Skid@users.noreply.github.com>
2019-01-21 03:48:30 -0500

    Delete deploy.sh

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-apt-get update -y && apt-get upgrade -y
-mkdir /tempdir
-git clone git://github.com/ps2dev/ps2toolchain.git && cd /tempdir/toolchain && ./toolchain-sudo.sh
-#git clone git://github.com/ps2dev/ps2eth.git /ps2dev/ps2eth \
-#&& make -C /ps2dev/ps2eth \
-git clone git://github.com/ps2dev/ps2-packer.git /tempdir/ps2-packer
-make install -C /tempdir/ps2-packer 
-rm -rf /tempdir
-cd /src && make
